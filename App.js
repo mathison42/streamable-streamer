c26473de6f5e9e7ef04a412f23d87283140438c0
mathison42 <mathison42@gmail.com>
2018-01-26 23:58:53 -0500

    Orientation Recognition and Clip List
    - Clip list only shows when screen is vertical
    - Clip list buttons will link to appropriate video

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PopUp from './native/components/PopUp';
 import StreamButton from './native/components/StreamButton';
-import { StyleSheet, Text, View, WebView, Modal, TouchableHighlight } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, WebView, Modal, TouchableHighlight, FlatList, Button } from 'react-native';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
 
+    const {width, height} = Dimensions.get('window')
     this.state = {
-        clipNum: Math.floor(Math.random() * clips.length)
+        clipNum: Math.floor(Math.random() * clips.length),
+        vertical: height > width ? true : false
     }
 
   }
+
+  onLayout(e) {
+    const {width, height} = Dimensions.get('window')
+    console.log(width, height)
+    if (height >= width) {
+      this.setState({ vertical: true })
+    } else {
+      this.setState({ vertical: false })
+    }
+  }
+
 
   nextClip = () => {
     let newNum = this.state.clipNum + 1;
@@ -32,7 +45,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-        <View style={styles.container}>
+        <View onLayout={this.onLayout.bind(this)} style={styles.container}>
 
           {/* Video */}
           <WebView source={{uri: clips[this.state.clipNum].streamable}} />
@@ -44,6 +57,19 @@ export default class App extends React.Component {
           {/* Description */}
           <PopUp text={clips[this.state.clipNum].description} />
 
+          { this.state.vertical
+            ?
+            /* Clip List */
+            <View style={styles.container}>
+              <FlatList
+                data={clips}
+                renderItem={({item}) => <Button style={styles.item} title={item.streamable} onPress={()=>this.setState({ clipNum: clips.indexOf(item) })}></Button>}
+              />
+            </View>
+            :
+            null
+          }
+
         </View>
     );
   }
@@ -51,12 +77,19 @@ export default class App extends React.Component {
 
 const clips = [
   {
+    key: 't8x2h',
     streamable: 'https://streamable.com/s/t8x2h/yexfin',
     description: 'Lost at orbiting. The initial angle is far better than where she orbits.'
   },
   {
+    key: 'rkofc',
     streamable: 'https://streamable.com/s/rkofc/zpjkyr',
     description: 'Fakes - Flick Shimmy!'
+  },
+  {
+    key: 'teggv',
+    streamable: 'https://streamable.com/s/teggv/wnkxmr',
+    description: 'Joke - Elite Mark'
   },
 ]
 
